08e59756c01b3694dd27f714662343c89b712bd4
julietabritoxmail.com <julietabritox@mail.com>
2025-09-20 23:23:46 -0300

    Borro todo el contenido de la rama

--- Frontend/src/Routers/router.js
+++
@@ -1,5 +0,0 @@
-
-export const BASE_URL = "http://localhost:3000";
-export const HOME_ESTUDIANTES = `${BASE_URL}/estudiantes`;
-export const HOME_CURSOS = `${BASE_URL}/cursos`;
-export const HOME_INSCRIPCION = `${BASE_URL}/inscripcion`;
